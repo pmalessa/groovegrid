de230e872504642d1c6cfb2e129e27f9d5711f8d
Christian Dibbern <chris.ed.apple@googlemail.com>
2019-01-05 00:24:22 +0100

    New class for Game Stateadded

---
+++ Software/groovegrid_v2/GameState.cpp
@@ -0,0 +1,16 @@
+/*
+ * GameState.cpp
+ *
+ *  Created on: 05.01.2019
+ *      Author: Chris
+ */
+
+#include "GameState.h"
+
+class GameState {
+	GameState() {
+
+	}
+};
+
+
---
+++ Software/groovegrid_v2/GameState.h
@@ -0,0 +1,15 @@
+/*
+ * GameState.h
+ *
+ *  Created on: 05.01.2019
+ *      Author: Chris
+ */
+
+
+
+/*class GameState {
+public:
+	GameState();
+};
+ */
+
