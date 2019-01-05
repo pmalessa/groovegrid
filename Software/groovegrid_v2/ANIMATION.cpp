0d9548fc0b57803ed2a9102f308f21abab3f9664
Phil Malessa <phil.malessa@tuhh.de>
2019-01-05 01:09:13 +0100

    abstracted the LED code from the rest

--- Software/groovegrid_v2/ANIMATION.cpp
+++ Software/groovegrid_v2/ANIMATION.cpp
@@ -4,9 +4,24 @@
  *  Created on: 22.09.2017
  *      Author: pmale
  */
+#include "LED.h"
 #include"ANIMATION.h"
 
-void ANIMATION_randomPixels(Adafruit_NeoMatrix *matrix)
+void ANIMATION_vBoot()
+{
+	for (uint8_t i=0; i < YMAX; i++) {
+		LED_vDrawLine(0, i, XMAX-1, i, LED_u16Color(255, 0, 0));
+		LED_vShow();
+		delay(100);
+	}
+	for (uint8_t i=0; i < YMAX; i++) {
+		LED_vDrawLine(0, i, XMAX-1, i, LED_u16Color(0, 0, 0));
+		LED_vShow();
+		delay(100);
+	}
+}
+
+void ANIMATION_randomPixels()
 {
 	static uint32_t wait = 0;
 
@@ -14,11 +29,11 @@ void ANIMATION_randomPixels(Adafruit_NeoMatrix *matrix)
 	{
 		if(rand()%2)	//clear or draw
 		{
-			matrix->drawPixel(rand()%4, rand()%4, matrix->Color(rand()%256, rand()%256, rand()%256));
+			LED_vDrawPixel(rand()%4, rand()%4, LED_u16Color(rand()%256, rand()%256, rand()%256));
 		}
 		else
 		{
-			matrix->drawPixel(rand()%4, rand()%4, matrix->Color(0, 0, 0));
+			LED_vDrawPixel(rand()%4, rand()%4, LED_u16Color(0, 0, 0));
 		}
 
 		wait = rand()%300;
@@ -29,7 +44,7 @@ void ANIMATION_randomPixels(Adafruit_NeoMatrix *matrix)
 	}
 }
 
-void ANIMATION_randomLines(Adafruit_NeoMatrix *matrix)
+void ANIMATION_randomLines()
 {
 	static uint32_t wait = 0;
 
@@ -37,11 +52,11 @@ void ANIMATION_randomLines(Adafruit_NeoMatrix *matrix)
 	{
 		if(rand()%2)
 		{
-			matrix->drawLine(rand()%4,rand()%4,rand()%4,rand()%4,matrix->Color(rand()%256, rand()%256, rand()%256));
+			LED_vDrawLine(rand()%4,rand()%4,rand()%4,rand()%4,LED_u16Color(rand()%256, rand()%256, rand()%256));
 		}
 		else
 		{
-			matrix->drawLine(rand()%4,rand()%4,rand()%4,rand()%4,matrix->Color(0, 0, 0));
+			LED_vDrawLine(rand()%4,rand()%4,rand()%4,rand()%4,LED_u16Color(0, 0, 0));
 		}
 
 		wait = rand()%300;
@@ -50,4 +65,29 @@ void ANIMATION_randomLines(Adafruit_NeoMatrix *matrix)
 	{
 		wait--;
 	}
+}
+
+void ANIMATION_vRunner()
+{
+	static uint32_t wait = 30000;
+	static uint8_t state = 0;
+	if(wait == 0)
+	{
+		state++;	//switch Animation every 10 seconds
+		LED_vClear();
+		wait = 30000;
+	}
+	wait--;
+	switch (state) {
+		case 0:
+			ANIMATION_randomPixels();
+			break;
+		case 1:
+			ANIMATION_randomLines();
+			break;
+		default:
+			state = 0;
+			break;
+	}
+	LED_vShow();
 }
