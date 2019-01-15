d9b95c6eb00e9b83e003923114bef45f62cc5163
Christian Dibbern <chris.ed.apple@googlemail.com>
2019-01-15 21:22:26 +0100

    Flutter project for mobile app created

---
+++ MobileApp/groove_grid/android/app/src/main/java/com/groovegrid/groovegrid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.groovegrid.groovegrid;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
