468ff79264a2a006c23a6e44d9ad87be7e8cf961
Christian Dibbern <chris.ed.apple@googlemail.com>
2019-01-17 22:30:16 +0100

    Separate Views for selecting Games and animations created

--- MobileApp/groove_grid/lib/main.dart
+++ MobileApp/groove_grid/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'bluetooth.dart';
+import 'controls.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
@@ -25,13 +26,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,37 +46,17 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _labelText = "";
+class _HomePageState extends State<HomePage> {
+  String _tab1LabelText = "First Tab";
+  String _tab2LabelText = "Second Tab";
+  
 
-  void _incrementCounter() {
+  void _setTab1Label(String text) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      print("Incrementing Counter");
-      _labelText = "";
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      print("Decrementing Counter");
-      _labelText = "";
-      _counter--;
-    });
-  }
-
-  void _setLabel(String text) {
-    setState(() {
-      _labelText = text;
+      _tab1LabelText = text;
     });
   }
 
@@ -100,14 +81,6 @@ class _MyHomePageState extends State<MyHomePage> {
               );
             },
           ),
-          /*FlatButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => BluetoothSettings()),
-              );
-            },
-            child: Text("Settings")),*/
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
@@ -119,94 +92,21 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children: [
-            GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: () => print("Tap Gesture recognized!"),
-              onHorizontalDragStart: (DragStartDetails details) async {
-                print("Horizontal Drag Started!");
-                print(details);
-              },
-              onHorizontalDragEnd: (DragEndDetails details) async {
-                var velocity = details.primaryVelocity;
-                print("Horitiontal Gesture recognized with speed $velocity");
-                if (velocity < -2) {
-                  _setLabel("Left swipe detected!");
-                  FlutterBluetoothSerial.instance.write('l');
-                } else if (velocity > 2) {
-                  _setLabel("Right swipe detected!");
-                  FlutterBluetoothSerial.instance.write('r');
-                } else {
-                  _setLabel("Horizontal swipe detected!");
-                }
-              },
-              onHorizontalDragCancel: () => (print("Vertical Drag cancelled!")),
-              onVerticalDragEnd: (DragEndDetails details) async {
-                var velocity = details.primaryVelocity;
-                print("Vertical Gesture recognized with speed $velocity");
-                if (velocity < -2) {
-                  _setLabel("Up swipe detected!");
-                  FlutterBluetoothSerial.instance.write('u');
-                } else if (velocity > 2) {
-                  _setLabel("Down swipe detected!");
-                  FlutterBluetoothSerial.instance.write('d');
-                } else {
-                  _setLabel("Vertical swipe detected!");
-                }
-              },
-              child: Center(
-                // Center is a layout widget. It takes a single child and positions it
-                // in the middle of the parent.
-                child: Column(
-                  // Column is also layout widget. It takes a list of children and
-                  // arranges them vertically. By default, it sizes itself to fit its
-                  // children horizontally, and tries to be as tall as its parent.
-                  //
-                  // Invoke "debug painting" (press "p" in the console, choose the
-                  // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                  // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                  // to see the wireframe for each widget.
-                  //
-                  // Column has various properties to control how it sizes itself and
-                  // how it positions its children. Here we use mainAxisAlignment to
-                  // center the children vertically; the main axis here is the vertical
-                  // axis because Columns are vertical (the cross axis would be
-                  // horizontal).
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      _labelText == ""
-                          ? 'You have pushed the button this many times:'
-                          : _labelText,
-                    ),
-                    Text(
-                      _labelText == "" ? '$_counter' : "",
-                      style: Theme.of(context).textTheme.display1,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Text("Second Page"),
+            Center(child: Text(_tab1LabelText)),
+            Center(child: Text(_tab2LabelText)),
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SwipeControlsView(title: 'Animations/2048',)),
+            );
+          },
+          tooltip: 'Play',
+          child: Icon(Icons.play_arrow),
         ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
 }
-
-//class BluetoothSettings extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("Bluetooth Settings"),
-//      ),
-//      body: null,
-//    );
-//  }
-//}
