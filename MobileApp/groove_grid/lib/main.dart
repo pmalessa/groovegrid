b9ee8f0dbbda7275744cfd6c7a6dca02e5315677
Christian Dibbern <chris.ed.apple@googlemail.com>
2019-01-17 21:55:51 +0100

    Tab Bar added

--- MobileApp/groove_grid/lib/main.dart
+++ MobileApp/groove_grid/lib/main.dart
@@ -72,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter--;
     });
   }
+
   void _setLabel(String text) {
     setState(() {
       _labelText = text;
@@ -86,91 +87,114 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        leading: FlatButton(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.bluetooth),
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => BluetoothSettings()),
               );
             },
-            child: Text("Settings")),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () => print("Tap Gesture recognized!"),
-        onHorizontalDragStart: (DragStartDetails details) async {
-          print("Horizontal Drag Started!");
-          print(details);
-        },
-        onHorizontalDragEnd:(DragEndDetails details) async {
-          var velocity = details.primaryVelocity;
-          print("Horitiontal Gesture recognized with speed $velocity");
-          if (velocity < -2) {
-            _setLabel("Left swipe detected!");
-            FlutterBluetoothSerial.instance.write('l');
-          } else if (velocity > 2){
-            _setLabel("Right swipe detected!");
-            FlutterBluetoothSerial.instance.write('r');
-          } else {
-            _setLabel("Horizontal swipe detected!");
-          }
-        },
-        onHorizontalDragCancel: () => (print("Vertical Drag cancelled!")),
-        onVerticalDragEnd: (DragEndDetails details) async {
-          var velocity = details.primaryVelocity;
-          print("Vertical Gesture recognized with speed $velocity");
-          if (velocity < -2) {
-            _setLabel("Up swipe detected!");
-            FlutterBluetoothSerial.instance.write('u');
-          } else if (velocity > 2){
-            _setLabel("Down swipe detected!");
-            FlutterBluetoothSerial.instance.write('d');
-          } else {
-            _setLabel("Vertical swipe detected!");
-          }
-        },
-        child: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                _labelText == ""? 'You have pushed the button this many times:': _labelText,
-              ),
-              Text(
-                _labelText == ""? '$_counter': "",
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
           ),
+          /*FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BluetoothSettings()),
+              );
+            },
+            child: Text("Settings")),*/
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.bubble_chart)),
+                Tab(icon: Icon(Icons.videogame_asset)),
+              ]),
         ),
+        body: TabBarView(
+          children: [
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () => print("Tap Gesture recognized!"),
+              onHorizontalDragStart: (DragStartDetails details) async {
+                print("Horizontal Drag Started!");
+                print(details);
+              },
+              onHorizontalDragEnd: (DragEndDetails details) async {
+                var velocity = details.primaryVelocity;
+                print("Horitiontal Gesture recognized with speed $velocity");
+                if (velocity < -2) {
+                  _setLabel("Left swipe detected!");
+                  FlutterBluetoothSerial.instance.write('l');
+                } else if (velocity > 2) {
+                  _setLabel("Right swipe detected!");
+                  FlutterBluetoothSerial.instance.write('r');
+                } else {
+                  _setLabel("Horizontal swipe detected!");
+                }
+              },
+              onHorizontalDragCancel: () => (print("Vertical Drag cancelled!")),
+              onVerticalDragEnd: (DragEndDetails details) async {
+                var velocity = details.primaryVelocity;
+                print("Vertical Gesture recognized with speed $velocity");
+                if (velocity < -2) {
+                  _setLabel("Up swipe detected!");
+                  FlutterBluetoothSerial.instance.write('u');
+                } else if (velocity > 2) {
+                  _setLabel("Down swipe detected!");
+                  FlutterBluetoothSerial.instance.write('d');
+                } else {
+                  _setLabel("Vertical swipe detected!");
+                }
+              },
+              child: Center(
+                // Center is a layout widget. It takes a single child and positions it
+                // in the middle of the parent.
+                child: Column(
+                  // Column is also layout widget. It takes a list of children and
+                  // arranges them vertically. By default, it sizes itself to fit its
+                  // children horizontally, and tries to be as tall as its parent.
+                  //
+                  // Invoke "debug painting" (press "p" in the console, choose the
+                  // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                  // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                  // to see the wireframe for each widget.
+                  //
+                  // Column has various properties to control how it sizes itself and
+                  // how it positions its children. Here we use mainAxisAlignment to
+                  // center the children vertically; the main axis here is the vertical
+                  // axis because Columns are vertical (the cross axis would be
+                  // horizontal).
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      _labelText == ""
+                          ? 'You have pushed the button this many times:'
+                          : _labelText,
+                    ),
+                    Text(
+                      _labelText == "" ? '$_counter' : "",
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Text("Second Page"),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
