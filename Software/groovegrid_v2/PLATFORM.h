5c0861b1bd701fcfb95cb7191d63b060bb7d382a
Phil Malessa <phil.malessa@tuhh.de>
2019-04-13 16:24:21 +0200

    Adding a DeltaTimer

--- Software/groovegrid_v2/PLATFORM.h
+++ Software/groovegrid_v2/PLATFORM.h
@@ -10,17 +10,19 @@
 
 #include "Arduino.h"
 
+#define FRAMERATE_MS 33	//33ms per frame -> 30fps
+
 // PLATFORM DEFINITION
-#define DOOR16
-//#define TABLE
+//#define DOOR16
+#define TABLE
 
 #if defined(DOOR16)
 #define XMAX 4
 #define YMAX 4
 #define DATA_PIN 6
 #elif defined(TABLE)
-#define XMAX 42
-#define YMAX 2
+#define XMAX 4
+#define YMAX 4
 #define DATA_PIN 32
 #endif
 
--- Software/groovegrid_v2/utils/GrooveAnimation.h
+++ Software/groovegrid_v2/utils/GrooveAnimation.h
@@ -10,15 +10,18 @@
 
 #include "../PLATFORM.h"
 #include "GrooveApp.h"
+#include "../driver/DeltaTimer.h"
 
 class GrooveAnimation : public GrooveApp
 {
 	//An Animation does not need to implement all functions of a GroovegridApp
 public:
-	GrooveAnimation(GridTile *tile):GrooveApp(tile){};
+	GrooveAnimation(GridTile *tile):GrooveApp(tile){frameTimer.setTimeStep(FRAMERATE_MS);};
 	virtual ~GrooveAnimation(){};
 	virtual void run() = 0;					//has to be overridden by Animation loop
 	void onInput(char *data){UNUSED(data);};
+private:
+	DeltaTimer frameTimer;
 };
 
 
