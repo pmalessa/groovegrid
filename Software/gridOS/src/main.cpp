764ea5b5680aac41547b131f54e611b9584015be
Phil Malessa <phil.malessa@tuhh.de>
2019-10-31 18:40:37 +0100

    removing unused buffer

--- Software/gridOS/src/main.cpp
+++ Software/gridOS/src/main.cpp
@@ -5,7 +5,6 @@
 extern "C" void app_main()
 {
 	const char* idleTaskTag = "IdleTask";
-	char buf[3000]; //40 bytes per Task necessary
 	delay(3000);
 	static MainLoop& mainLoop = MainLoop::getInstance();
 	UNUSED(mainLoop);
@@ -13,9 +12,6 @@ extern "C" void app_main()
 	while(1)
 	{
 		ESP_LOGI(idleTaskTag,"Heap: %i",xPortGetFreeHeapSize());
-		vTaskDelay(3000);
-		//vTaskList(buf);
-		//ESP_LOGI(idleTaskTag,"%s",buf);
 		vTaskDelay(3000);
 	}
 }
